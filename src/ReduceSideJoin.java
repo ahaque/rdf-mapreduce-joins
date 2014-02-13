8f6b485b9270dfabd505fee118595cd7918ded81
Albert Haque <ahaque@cs.utexas.edu>
2014-02-13 16:05:26 -0600

    Changes to mapper class.

--- src/ReduceSideJoin.java
+++ src/ReduceSideJoin.java
@@ -1,23 +1,16 @@
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 
-import BSBMQuery7.Counters;
 
 public class ReduceSideJoin {
 
@@ -28,16 +21,22 @@ public class ReduceSideJoin {
 		// args[0] = zookeeper
 		// args[1] = hbase table name
 		
-		Job job = run(args);
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		String USAGE_MSG = "  Arguments: <zk quorum>";
+
+		if (args == null || args.length != 1) {
+			System.out.println(USAGE_MSG);
+			System.exit(0);
+		}
+
+		Job job = startJob(args);
 	}
 	
 	@SuppressWarnings("deprecation")
-	public static Job run(String[] args) throws IOException {
+	public static Job startJob(String[] args) throws IOException {
 		
 		Configuration hConf = HBaseConfiguration.create(new Configuration());
 	    hConf.set("hbase.zookeeper.quorum", args[0]);
-	    hConf.set("scan.table", args[1]);
+	    hConf.set("scan.table", HBASE_TABLE_NAME);
 	    hConf.set("hbase.zookeeper.property.clientPort", "2181");
 
 	    Scan scan = new Scan();
@@ -56,11 +55,13 @@ public class ReduceSideJoin {
 				null,             // mapper output key
 				null,             // mapper output value
 				job);
-				job.setOutputFormatClass(NullOutputFormat.class);   // because we aren't emitting anything from mapper
+		job.setOutputFormatClass(NullOutputFormat.class);   // because we aren't emitting anything from mapper
 
-		boolean b = job.waitForCompletion(true);
-		if (!b) { throw new IOException("error with job!"); }
-		
+		try {
+			System.exit(job.waitForCompletion(true) ? 0 : 1);
+		} catch (ClassNotFoundException e) { e.printStackTrace(); }
+		  catch (InterruptedException e) { e.printStackTrace();}
+
 		return job;
 	}
 	
