3f7f84900badbba1930dbab61018046a731d6f35
Albert Haque <ahaque@cs.utexas.edu>
2014-02-13 16:00:24 -0600

    Added new Scan mapper

--- src/ReduceSideJoin.java
+++ src/ReduceSideJoin.java
@@ -1,14 +1,33 @@
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
+
+import BSBMQuery7.Counters;
 
 public class ReduceSideJoin {
 
+	static String HBASE_TABLE_NAME = "rdf1";
+	
 	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+		
+		// args[0] = zookeeper
+		// args[1] = hbase table name
+		
 		Job job = run(args);
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
@@ -31,19 +50,25 @@ public class ReduceSideJoin {
 		scan.setCacheBlocks(false);
 		
 		TableMapReduceUtil.initTableMapperJob(
-				  tableName,        // input HBase table name
-				  scan,             // Scan instance to control CF and attribute selection
-				  MyMapper.class,   // mapper
-				  null,             // mapper output key
-				  null,             // mapper output value
-				  job);
+				HBASE_TABLE_NAME,        // input HBase table name
+				scan,             // Scan instance to control CF and attribute selection
+				MyMapper.class,   // mapper
+				null,             // mapper output key
+				null,             // mapper output value
+				job);
 				job.setOutputFormatClass(NullOutputFormat.class);   // because we aren't emitting anything from mapper
 
-				boolean b = job.waitForCompletion(true);
-				if (!b) {
-				  throw new IOException("error with job!");
+		boolean b = job.waitForCompletion(true);
+		if (!b) { throw new IOException("error with job!"); }
 		
 		return job;
 	}
 	
+	public static class MyMapper extends TableMapper<Text, Text> {
+
+		  public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
+		    // process data for the row from the Result instance.
+		   }
+		}
+		    
 }
