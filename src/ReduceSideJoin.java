d6132515a89667806adca7c2d244ec25bf8408fe
Albert Haque <ahaque@cs.utexas.edu>
2014-03-13 15:11:09 -0400

    Added BSBM query 1

--- src/ReduceSideJoin.java
+++ src/ReduceSideJoin.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
@@ -29,35 +30,42 @@ import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 
 public class ReduceSideJoin {
+	
+	// Query Information
+	private static String ProductFeature1 = "hello";
+	private static String ProductFeature2 = "hello";
+	private static String ProductType = "hello";
+	private static double x = 0.0;
 
-	private static String HBASE_TABLE_NAME = "rdf1";
-	private byte[] COLUMN_FAMILY_AS_BYTES = "p".getBytes();
+	private byte[] COLUMN_FAMILY_AS_BYTES = "o".getBytes();
 	private byte[] fakeValue = Bytes.toBytes("DOESNOTEXIST");
+	private byte[] productName = Bytes.toBytes("product_name_here");
 	
 	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
 
-		//String USAGE_MSG = "  Arguments: <zk quorum>";
+		String USAGE_MSG = "  Arguments: <table name> <zk quorum>";
 
-//		if (args == null || args.length != 1) {
-//			System.out.println(USAGE_MSG);
-//			System.exit(0);
-//		}
+		if (args == null || args.length != 2) {
+			System.out.println("\n" + USAGE_MSG);
+			System.out.println("You entered " + args.length + " arguments.");
+			System.exit(0);
+		}
 		startJob(args);
 	}
 	
 	@SuppressWarnings("deprecation")
 	public static Job startJob(String[] args) throws IOException {
 		
-		// args[0] = zookeeper
-		// args[1] = hbase table name
+		// args[0] = hbase table name
+		// args[1] = zookeeper
 		
 		Configuration hConf = HBaseConfiguration.create(new Configuration());
-	    hConf.set("hbase.zookeeper.quorum", "localhost");
-	    hConf.set("scan.table", HBASE_TABLE_NAME);
+	    hConf.set("hbase.zookeeper.quorum", args[1]);
+	    hConf.set("scan.table", args[0]);
 	    hConf.set("hbase.zookeeper.property.clientPort", "2181");
 
 	    Scan scan = new Scan();
-	    scan.setFilter(rowColBloomFilter());
+	    //scan.setFilter(rowColBloomFilter());
 		
 		Job job = new Job(hConf);
 		job.setJobName("Reduce Side Join");
@@ -68,7 +76,7 @@ public class ReduceSideJoin {
 		
 		// Mapper settings
 		TableMapReduceUtil.initTableMapperJob(
-				HBASE_TABLE_NAME,        // input HBase table name
+				args[0],        // input HBase table name
 				scan,             // Scan instance to control CF and attribute selection
 				ReduceSideJoin_Mapper.class,   // mapper
 				Text.class,         // mapper output key
@@ -79,7 +87,7 @@ public class ReduceSideJoin {
 		job.setReducerClass(ReduceSideJoin_Reducer.class);    // reducer class
 		job.setNumReduceTasks(1);    // at least one, adjust as required
 		
-		FileOutputFormat.setOutputPath(job, new Path("output/2014-03-12_output.txt"));
+		FileOutputFormat.setOutputPath(job, new Path("output/2014-03-13"));
 
 		try {
 			System.exit(job.waitForCompletion(true) ? 0 : 1);
@@ -89,41 +97,29 @@ public class ReduceSideJoin {
 		return job;
 	}
 	
-	private static Filter rowColBloomFilter() {
-		// the base BD filter, either offers or reviews must have the product as column to be considered
-		Filter filter = new BloomFilterSemiJoinFilter(COL_NAME_ROW,
-		BSBMDataSetProcessor.COLUMN_FAMILY_AS_BYTES, new BloomFilterSemiJoinFilter.StaticRowBloomFilterLookupKeyBuilder(productName));
-		return new FilterList(FilterList.Operator.MUST_PASS_ALL, filter, new FilterList(FilterList.Operator.MUST_PASS_ONE, reviewsFilter(), offersFilter()));
-	}
+//	private static Filter rowColBloomFilter() {
+//		// the base BD filter, either offers or reviews must have the product as column to be considered
+//		Filter filter = new BloomFilterSemiJoinFilter(COL_NAME_ROW,
+//		BSBMDataSetProcessor.COLUMN_FAMILY_AS_BYTES, new BloomFilterSemiJoinFilter.StaticRowBloomFilterLookupKeyBuilder(productName));
+//		return new FilterList(FilterList.Operator.MUST_PASS_ALL, filter, new FilterList(FilterList.Operator.MUST_PASS_ONE, reviewsFilter(), offersFilter()));
+//	}
 	
 	private Filter reviewsFilter() {
 		// select only the reviews that have product as a column name
 		SingleColumnValueFilter filter = new SingleColumnValueFilter(
-				BSBMDataSetProcessor.COLUMN_FAMILY_AS_BYTES, productName,
+				COLUMN_FAMILY_AS_BYTES, productName,
 				CompareFilter.CompareOp.NOT_EQUAL, fakeValue);
 		filter.setFilterIfMissing(true);
 		return filter;
-
 	}
 
 	private Filter offersFilter() {
 		// select only the rows that have product as a column value.
-		SingleColumnValueFilter excludeIfNoProduct = new SingleColumnValueFilter(
+		SingleColumnValueFilter filter = new SingleColumnValueFilter(
 				COLUMN_FAMILY_AS_BYTES, productName,
 				CompareFilter.CompareOp.NOT_EQUAL, fakeValue);
-		excludeIfNoProduct.setFilterIfMissing(true);
-
-		// of those select only the ones for whom the BF matches vendor
-		// (dc_publisher) -> country
-		BloomFilterSemiJoinFilter bfFilter = new BloomFilterSemiJoinFilter(
-				COL_NAME_ROW,
-				BSBMDataSetProcessor.COLUMN_FAMILY_AS_BYTES,
-				new ColumnPrefixFilter(productName),
-				new BloomFilterSemiJoinFilter.StaticColumnBloomFilterLookupKeyBuilder(
-						country));
-
-		return new FilterList(FilterList.Operator.MUST_PASS_ALL,
-				excludeIfNoProduct, bfFilter);
+		filter.setFilterIfMissing(true);
+		return filter;
 	}
 	
 	public static class ReduceSideJoin_Mapper extends TableMapper<Text, IntWritable> {
@@ -132,13 +128,25 @@ public class ReduceSideJoin {
 		private Text text = new Text();
 
 		public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
+			// BSBM Query 1. Subject must satisfy all triple patterns
+			// Non equality checks in the query appear as true here
+			boolean[] queryTriplePatterns = new boolean[6];
+			queryTriplePatterns[0] = true;
+			queryTriplePatterns[1] = false;
+			queryTriplePatterns[2] = false;
+			queryTriplePatterns[3] = false;
+			queryTriplePatterns[4] = true;
+			queryTriplePatterns[5] = false;
+			
 			// Get all key-values for a row
 		    Cell[] cells = value.rawCells();
+		    StringBuilder predicate = new StringBuilder();
+		    StringBuilder object = new StringBuilder();
 		    for (Cell kv : cells) {
-		    	String object = new String(CellUtil.cloneQualifier(kv));
-		    	text.set(object);
-		    	context.write(text, ONE);
+
 		    }
+		    text.set(object.toString());
+	    	context.write(text, ONE);
 		}
 	}
 	
