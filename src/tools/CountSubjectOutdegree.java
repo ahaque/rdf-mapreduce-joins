bebdf56e743cc6a30fd7f07c645a59b2feffca7c
Albert Haque <ahaque@cs.utexas.edu>
2014-04-17 01:14:10 -0500

    Added NT to KeyList transformer

--- src/tools/CountSubjectOutdegree.java
+++ src/tools/CountSubjectOutdegree.java
@@ -30,18 +30,15 @@ public class CountSubjectOutdegree {
 		}
 	}
 
-	 public static class Reduce extends Reducer<Text, IntWritable, Text, Text> {
+	 public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 		    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
 		      throws IOException, InterruptedException {
-		        /*
 		    	int sum = 0;
 		        for (IntWritable val : values) {
 		            sum += val.get();
 		        }
 		        context.write(key, new IntWritable(sum));
-		        */
-		        context.write(key, new Text(""));
 		    }
 		 }
 
