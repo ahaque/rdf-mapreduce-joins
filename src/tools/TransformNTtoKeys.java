8d288b7591e3cec5c08a048ae55d52fef672100b
Albert Haque <ahaque@cs.utexas.edu>
2014-04-17 02:01:20 -0500

    Changes to key extractor

--- src/tools/TransformNTtoKeys.java
+++ src/tools/TransformNTtoKeys.java
@@ -3,30 +3,45 @@ package tools;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import tools.BSBMDataSetProcessor.Triple;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TransformNTtoKeys extends Configured implements Tool {
 
-  public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-      BSBMDataSetProcessor.Triple triple = (Triple) BSBMDataSetProcessor.process(value.toString());
-      context.write(new Text(triple.subject), new Text(triple.subject));
-    }
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		@Override
+		protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+			List<Triple> tripleList = BSBMDataSetProcessor.process(value.toString());
+			for (Triple t : tripleList) {
+				context.write(new Text(t.subject), one);
+			}
+		}
+
+  }
+	
+	public static class Reduce1 extends Reducer<Text, IntWritable, Text, Text> {
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		protected void reduce(Text key, Iterable<IntWritable> value, Mapper.Context context) throws IOException, InterruptedException {
+			context.write(key, new Text(""));
+		}
 
   }
 
@@ -48,10 +63,13 @@ public class TransformNTtoKeys extends Configured implements Tool {
     job.setJobName("TransformNTtoKeys");
     job.setJarByClass(TransformNTtoKeys.class);
     job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce1.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
-    job.setNumReduceTasks(0);
-    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+    job.setNumReduceTasks(1);
+    job.setOutputFormatClass(TextOutputFormat.class);
     job.setInputFormatClass(TextInputFormat.class);
     return job.waitForCompletion(true) ? 0 : 1;
 
