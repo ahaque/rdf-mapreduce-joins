bff92fb7e252f9b26623ee0d4963365bd2663b78
Albert Haque <ahaque@cs.utexas.edu>
2014-05-09 04:18:55 -0500

    Started counter on LUBM Q2 Repartition, fixed Q2 stage1 numReducers

--- src/main/java/lubm/repartition/RepartitionLUBMQ2.java
+++ src/main/java/lubm/repartition/RepartitionLUBMQ2.java
@@ -25,6 +25,7 @@ import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -39,13 +40,25 @@ import bsbm.repartition.CompositePartitioner;
 import bsbm.repartition.CompositeSortComparator;
 import bsbm.sortmerge.KeyValueArrayWritable;
 import bsbm.sortmerge.SharedServices;
+import lubm.sortmerge.LUBMSharedServices.LUBMQ2_COUNTERS;
 
 
 public class RepartitionLUBMQ2 {
 	
 	// Begin Query Information
-	
 	// End Query Information
+	
+	private static Counter univsIn;
+	private static Counter univsJoinedOn;
+	private static Counter depsIn;
+	private static Counter depsJoinedOn;
+	private static Counter univsDepsJoinTotal;
+	private static Counter setupTuplesIn;
+    private static Counter gradStudentsIn;
+    private static Counter gradStudentsJoinedOn;
+    private static Counter univDepGradStudentJoinTotal;
+    private static Counter mapTuplesIn;
+    private static Counter mapTuplesOut;
 		
 	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
 
@@ -136,6 +149,21 @@ public class RepartitionLUBMQ2 {
 	
 	public static class Stage1_RepartitionMapper extends TableMapper<CompositeKeyWritable, KeyValueArrayWritable> {
 		
+        protected void setup(Context context) throws IOException, InterruptedException {
+            
+			univsIn = context.getCounter(LUBMQ2_COUNTERS.UNIVERSITIES_IN);
+			univsJoinedOn = context.getCounter(LUBMQ2_COUNTERS.UNIVERSITIES_JOINED_ON);
+			depsIn = context.getCounter(LUBMQ2_COUNTERS.DEPARTMENTS_IN);
+			depsJoinedOn = context.getCounter(LUBMQ2_COUNTERS.DEPARTMENTS_JOINED_ON);
+			univsDepsJoinTotal = context.getCounter(LUBMQ2_COUNTERS.UNIVERSITIES_DEPARTMENTS_JOIN_TOTAL);
+			gradStudentsIn = context.getCounter(LUBMQ2_COUNTERS.GRAD_STUDENTS_IN);
+			gradStudentsJoinedOn = context.getCounter(LUBMQ2_COUNTERS.GRAD_STUDENTS_JOINED_ON);
+			univDepGradStudentJoinTotal = context.getCounter(LUBMQ2_COUNTERS.UNIV_DEP_GRA_STUDENT_JOIN_TOTAL);
+//			setupTuplesIn = context.getCounter(LUBM_COUNTERS.SETUP_TUPLES_IN);
+//			mapTuplesIn = context.getCounter(LUBM_COUNTERS.MAP_TUPLES_IN);
+//			mapTuplesOut = context.getCounter(LUBM_COUNTERS.MAP_TUPLES_OUT);
+        }
+		
 		public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
 		/* LUBM QUERY 2
 		   ----------------------------------------
@@ -151,8 +179,9 @@ public class RepartitionLUBMQ2 {
 		  [TP-06] ?X ub:undergraduateDegreeFrom ?Y}
 		   ---------------------------------------
 		 */
-			// Determine if this row is a grad student, university, or department
 			
+			mapTuplesIn.increment(1);
+			// Determine if this row is a grad student, university, or department
 			// TP-01, TP-02, TP-03
 			byte[] x_type = value.getValue(SharedServices.CF_AS_BYTES, Bytes.toBytes("ub_GraduateStudent"));
 			byte[] y_type = value.getValue(SharedServices.CF_AS_BYTES, Bytes.toBytes("ub_University"));
@@ -163,6 +192,7 @@ public class RepartitionLUBMQ2 {
 			
 			// If this row is a grad student
 			if (x_type != null) {
+				gradStudentsIn.increment(1);
 				// Emit TP-06
 				byte[] y_reducerKey = null;
 				byte[] z_reducerKey = null;
@@ -189,6 +219,7 @@ public class RepartitionLUBMQ2 {
 			
 			// If this row is a university
 			else if (y_type != null) {
+				univsIn.increment(1);
 				for (KeyValue kv : entireRowAsList) {
 					if (Arrays.equals(kv.getValue(), "rdf_type".getBytes())) {
 						toTransmit.add(kv);
@@ -199,6 +230,7 @@ public class RepartitionLUBMQ2 {
 			
 			// If this row is a department
 			else if (z_type != null) {
+				depsIn.increment(1);
 				for (KeyValue kv : entireRowAsList) {
 					if (Arrays.equals(kv.getValue(), "ub_subOrganizationOf".getBytes())) {
 						toTransmit.add(kv);
